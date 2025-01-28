94c81024c74308138d063ff0492713a04f02af44
Tunere Ereibor <etunere@gmail.com>
2025-01-28 10:36:34 +0100

    successfully configured next auth with google

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,1 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-
-
-export default NextAuth({
-  providers: [
-    // OAuth authentication providers...
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-    }),
-  ]
-})
+export { GET, POST } from "@/app/lib/auth";
--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
-import Image from "next/image";
+"use client"
+
+import { signIn } from "next-auth/react"  // Import directly from next-auth/react for client components
 
 export default function Home() {
   return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-md">Login with Google</button>
-      </div>
+    <div className="bg-blue-900 w-screen h-screen flex items-center justify-center">
+      <button
+        onClick={() => signIn("google")}
+        className="bg-white p-2 px-4 rounded-md"
+      >
+        Sign in with Google
+      </button>
     </div>
   );
 }
