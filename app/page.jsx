85fdf2a8d19b0350c3ad4478cd3103ad2fea1552
Tunere Ereibor <etunere@gmail.com>
2025-02-06 14:52:21 +0100

    properly updated

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex-grow">
       <div className="flex flex-col items-center">
-       <div className="bg-white">THIS HOME PAGE</div> 
+       <div className="bg-white">THIS HOME IS PAGE</div> 
       <button
         onClick={() => signIn("google")}
         className=" bg-red-400  text-white p-2 px-4 rounded-md"
